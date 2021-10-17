3f2abca381cc0083c64b65cc97866d52ea01a890
Patrick LeBlanc <patrick.m.leblanc@gmail.com>
2021-10-17 15:40:44 -0400

    Changed ratings matrix to have only movies from tag genome

--- Scripts/Generate_Ratings_Matrix.R
+++ Scripts/Generate_Ratings_Matrix.R
@@ -20,16 +20,41 @@ rm(ratings)
 rm(userIDs)
 rm(user_rows)
 
-#generate a ratings matrix for the specified users and all the movies
+#For ease of comparison, we want to find movies which have tag genome data
+#load genome scores
+#this assumes a certain directory structure where the movielens data is stored in a Data
+#directory which is in the same file as the github directory
+#also this assumes that we are in the Scripts directory
+genome = read.csv("../../Data/genome-scores.csv")
+#find all unique movies with tag genome data
+tag_movies = unique(genome$movieId)
+
+#manage memory
+rm(genome)
+
+#find all unique movies in the ratings data
 unique_movies = sort(unique(sub_ratings$movieId))
-rat_mat = matrix(0,nrow = length(users), ncol = length(unique_movies))
+#find the movies both in the ratings data and the tag data
+desired_movies = NULL
+for(i in 1:length(unique_movies)){
+  if(unique_movies[i] %in% tag_movies){
+    desired_movies = c(desired_movies,unique_movies[i])
+  }
+}
+
+#find the ratings matrix for the movies in the tag genome dataset
+rat_mat = matrix(0,nrow = length(users), ncol = length(desired_movies))
 for(i in 1:nrow(rat_mat)){
   user_rows = which(sub_ratings$userId == i)
   movies = sub_ratings$movieId[user_rows]
+  movies = movies[movies %in% desired_movies]
   for(j in 1:length(movies)){
-    rat_mat[i,which(unique_movies == movies[j])] = sub_ratings$rating[which(sub_ratings$movieId[user_rows] == movies[j])]
+    rat_mat[i,which(desired_movies == movies[j])] = sub_ratings$rating[which(sub_ratings$movieId[user_rows] == movies[j])]
   }
 }
+
+rownames(rat_mat) = users
+colnames(rat_mat) = desired_movies
 
 #save the ratings matrix for future use
 save(rat_mat,file="../Data Structures/rat_mat.rda")
