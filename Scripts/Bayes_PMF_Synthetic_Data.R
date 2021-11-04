c8c6a523f7fd705dd4024adc1f541a70b6ae4588
Patrick LeBlanc <patrick.m.leblanc@gmail.com>
2021-11-04 14:07:12 -0400

    Updated Gibbs sampler for Bayesian PMF
    If we factor R = UV^T, with appropriate priors on U and V, then I have a Gibbs sampler for U and its priors given V and a Gibbs sampler for V and its priors given U.  Currently, the synthetic dataset is full - there is zero sparsity.  Some experimentation indicates that the levels of sparsity present in the data could be a problem.

--- Scripts/Bayes_PMF_Synthetic_Data.R
+++ Scripts/Bayes_PMF_Synthetic_Data.R
@@ -8,7 +8,9 @@ set.seed(1)
 load("../Data Structures/cluster_mat.rda")
 
 #rename to indicator functions
-I_nm = cluster_mat[1:100,] #subset for time...
+I_nm = cluster_mat[1:100,1:1000] #subset for time...
+#make new indicator matrix that is full, to eliminate effects of empty data
+I_nm = matrix(1,nrow = 100, ncol = 100)
 
 # recover number of users N and number of items M
 N= nrow(I_nm)
@@ -105,8 +107,25 @@ mu_u = true_mu_u
 #initialize to truth
 U = true_U
 
+# #initialize Lambda_V at random
+# Lambda_V = rWishart(1,v_0,W_0)
+# Lambda_V = Lambda_V[,,1]
+# Lambda_V_inv = solve(Lambda_V)
+# #initialize to truth
+Lambda_V = true_Lambda_V
+Lambda_V_inv = solve(Lambda_V)
+
+# #initiailze mu_u at random
+# mu_v = rnorm(D)
+#initialize to truth
+mu_v = true_mu_v
+
 #initialize V at random
-#initailize to truth
+# V = matrix(0,nrow = M,ncol = D)
+# for(i in 1:nrow(V)){
+#   V[i,] = rnorm(D)
+# }
+# #initailize to truth
 V = true_V
 
 #set number of iterations
@@ -117,6 +136,8 @@ n_it = warmup + iterations
 #create data structures to store chains
 chain_mu_u_id = matrix(0,nrow= iterations, ncol = D)
 chain_U_ind = array(0,dim=c(iterations,N,D))
+chain_mu_v_id = matrix(0,nrow= iterations, ncol = D)
+chain_V_imd = array(0,dim=c(iterations,M,D))
 
 #################
 # Gibbs Sampler #
@@ -126,76 +147,138 @@ chain_U_ind = array(0,dim=c(iterations,N,D))
 
 for(it in 1:n_it){
 
+  # #####################
+  # # mu_u and Lambda_u #
+  # #####################
+  # 
+  # #find sufficient stats first
+  # U_mean = apply(U,2,mean)
+  # S = matrix(0,nrow = ncol(U),ncol = ncol(U))
+  # for(n in 1:N){
+  #   S = S + U[i,] %*% t(U[i,])
+  # }
+  # S = S/N
+  # 
+  # #find full conditional parameters for mu_u and Lambda_u
+  # beta_star = beta_0 + N
+  # v_star = v_0 + N
+  # avg = (beta_0*mu_u0  + N*U_mean)/(beta_star)
+  # W_0_star_inv = W_0_inv + N*S + beta_0*N/beta_star * (mu_u0 - U_mean) %*% t((mu_u0 - U_mean))
+  # W_0_star = solve(W_0_star_inv)
+  # 
+  # #draw Lambda_U first
+  # Lambda_U = rWishart(1,v_star,W_0)
+  # Lambda_U = Lambda_U[,,1]
+  # Lambda_U_inv = solve(Lambda_U)
+  # 
+  # #initiailze mu_u at random
+  # mu_u = chol(1/beta_star*Lambda_U_inv) %*% rnorm(D) + avg
+  # 
+  # #####
+  # # U #
+  # #####
+  # 
+  # for(n in 1:N){
+  #   
+  #   #find sufficient stats
+  #   S = matrix(0,nrow = D, ncol = D)
+  #   for(m in 1:M){
+  #     if((I_nm[n,m]>0) == TRUE){
+  #       S = S + V[m,] %*% t(V[m,])
+  #     }
+  #   }
+  #   
+  #   s = rep(0,D)
+  #   for(m in 1:M){
+  #     if((I_nm[n,m]>0) == TRUE){
+  #       s = s + V[m,]*R_nm[n,m]
+  #     }
+  #   }
+  #   #maybe I should rename them becaue S and s are similar..
+  #   
+  #   #find variance and mean for normal
+  #   covar = Lambda_U + alpha*S
+  #   covar_inv = solve(covar)
+  #   avg = covar_inv%*%(alpha*s + Lambda_U %*% mu_u )
+  #   
+  #   #draw U_n
+  #   U[n,] = chol(covar_inv) %*% rnorm(D) + avg
+  # }
+
   #####################
-  # mu_u and Lambda_u #
+  # mu_v and Lambda_v #
   #####################
-  
+
   #find sufficient stats first
-  U_mean = apply(U,2,mean)
-  S = matrix(0,nrow = ncol(U),ncol = ncol(U))
+  V_mean = apply(V,2,mean)
+  S = matrix(0,nrow = ncol(V),ncol = ncol(V))
   for(n in 1:N){
-    S = S + U[i,] %*% t(U[i,])
+    S = S + V[i,] %*% t(V[i,])
   }
-  S = S/N
+  S = S/M
 
   #find full conditional parameters for mu_u and Lambda_u
-  beta_star = beta_0 + N
-  v_star = v_0 + N
-  avg = (beta_0*mu_u0  + N*U_mean)/(beta_star)
-  W_0_star_inv = W_0_inv + N*S + beta_0*N/beta_star * (mu_u0 - U_mean) %*% t((mu_u0 - U_mean))
+  beta_star = beta_0 + M
+  v_star = v_0 + M
+  avg = (beta_0*mu_u0  + M*V_mean)/(beta_star)
+  W_0_star_inv = W_0_inv + M*S + beta_0*M/beta_star * (mu_u0 - V_mean) %*% t((mu_u0 - V_mean))
   W_0_star = solve(W_0_star_inv)
 
   #draw Lambda_U first
-  Lambda_U = rWishart(1,v_star,W_0)
-  Lambda_U = Lambda_U[,,1]
-  Lambda_U_inv = solve(Lambda_U)
+  Lambda_V = rWishart(1,v_star,W_0)
+  Lambda_V = Lambda_V[,,1]
+  Lambda_V_inv = solve(Lambda_V)
 
   #initiailze mu_u at random
-  mu_u = chol(1/beta_star*Lambda_U_inv) %*% rnorm(D) + avg
-
-  #####
-  # U #
-  #####
+  mu_v = chol(1/beta_star*Lambda_V_inv) %*% rnorm(D) + avg
   
-  for(n in 1:N){
-    
+  #####
+  # V #
+  #####
+
+  for(m in 1:M){
+
     #find sufficient stats
     S = matrix(0,nrow = D, ncol = D)
-    for(m in 1:M){
+    for(n in 1:N){
       if((I_nm[n,m]>0) == TRUE){
-        S = S + V[m,] %*% t(V[m,])
+        S = S + U[n,] %*% t(U[n,])
       }
     }
-    
+
     s = rep(0,D)
-    for(m in 1:M){
+    for(n in 1:N){
       if((I_nm[n,m]>0) == TRUE){
-        s = s + V[m,]*R_nm[n,m]
+        s = s + U[n,]*R_nm[n,m]
       }
     }
     #maybe I should rename them becaue S and s are similar..
-    
-    #find variance and mean for normal
-    covar = Lambda_U + alpha*S
-    covar_inv = solve(covar)
-    avg = covar_inv%*%(alpha*s + Lambda_U %*% mu_u )
-    
-    #draw U_n
-    U[n,] = chol(covar_inv) %*% rnorm(D) + avg
-  }
 
+    #find variance and mean for normal
+    covar = Lambda_V + alpha*S
+    covar_inv = solve(covar)
+    avg = covar_inv%*%(alpha*s + Lambda_V %*% mu_v )
+
+    #draw V_m
+    V[m,] = chol(covar_inv) %*% rnorm(D) + avg
+  }
+  
   ##########
   # Record #
   ##########
 
   #record the sampled values
   if(it>warmup){
-    #record mu_u
-    chain_mu_u_id[it - warmup,] = mu_u
-    
-    #record U
-    chain_U_ind[it - warmup,,] = U
-
+    # #record mu_u
+    # chain_mu_u_id[it - warmup,] = mu_u
+    # 
+    # #record U
+    # chain_U_ind[it - warmup,,] = U
+    #
+    #record mu_v
+    chain_mu_v_id[it - warmup,] = mu_v
+    # record V
+    chain_V_imd[it - warmup,,] = V
   }
 
   print(it)
@@ -204,23 +287,56 @@ for(it in 1:n_it){
 
 #posterior analysis
 
-#posterior mean for mu_u
-post_mu_u = apply(chain_mu_u_id,2,mean)
+# #posterior mean for mu_u
+# post_mu_u = apply(chain_mu_u_id,2,mean)
+# #chainplot
+# matplot(chain_mu_u_id,type="l")
+# for(d in 1:D){
+#   abline(h = true_mu_u[d])
+# }
+# #posterior mean vs truth
+# plot(true_mu_u,post_mu_u)
+# 
+# #posterior mean for U
+# post_U_nd = matrix(0,nrow = N,ncol = D)
+# for(n in 1:N){
+#   post_U_nd[n,] = apply(chain_U_ind[,n,],2,mean)
+# }
+# #chainplot
+# matplot(chain_U_ind[,n,],type="l")
+# #plot posterior mean vs truth
+# plot(true_U[,],post_U_nd[,])
+
+#posterior mean for mu_v
+post_mu_v = apply(chain_mu_v_id,2,mean)
 #chainplot
-matplot(chain_mu_u_id,type="l")
+matplot(chain_mu_v_id,type="l")
 for(d in 1:D){
-  abline(h = true_mu_u[d])
+  abline(h = true_mu_v[d])
 }
 #posterior mean vs truth
-plot(true_mu_u,post_mu_u)
+plot(true_mu_v,post_mu_v)
 
-#posterior mean for U
-post_U_nd = matrix(0,nrow = N,ncol = D)
-for(n in 1:N){
-  post_U_nd[n,] = apply(chain_U_ind[,n,],2,mean)
+#posterior mean for V
+post_V_md = matrix(0,nrow = M,ncol = D)
+for(m in 1:M){
+  post_V_md[m,] = apply(chain_V_imd[,m,],2,mean)
 }
 #chainplot
-matplot(chain_U_ind[,n,],type="l")
+matplot(chain_V_imd[,m,],type="l")
+#plot posterior mean vs truth
+num_rat = apply(I_nm,2,sum) #need to make sure we only consider movies with lots of data...
+plot(true_V[num_rat>10,],post_V_md[num_rat>10,])
 
-plot(true_U[,],post_U_nd[,])
 
+#test how posterior means do on prediction
+post_R_nm = matrix(0,nrow = N, ncol = M)
+for(i in 1:N){
+  for(j in 1:M){
+    if( (I_nm[i,j]>0) == TRUE){
+      post_R_nm[i,j] = sum(true_U[i,] * post_V_md[j,])
+    }
+  }
+}
+
+plot(R_nm[which(I_nm>0)], post_R_nm[which(I_nm>0)])
