8061fcde0da559547c7abb183d741264fd3b86fb
Patrick LeBlanc <patrick.m.leblanc@gmail.com>
2021-11-05 11:43:54 -0400

    Bug Fix
    Fixed a bug in updating Lambda_U and Lambda_V

--- Scripts/Bayes_PMF_Synthetic_Data.R
+++ Scripts/Bayes_PMF_Synthetic_Data.R
@@ -21,7 +21,7 @@ rm(cluster_mat)
 
 
 #set D
-D = 5
+D = 3
 
 #set hyperparameters
 mu_u0 = mu_v0 = rep(0,D)
@@ -132,14 +132,16 @@ mu_v = true_mu_v
 V = true_V
 
 #set number of iterations
-warmup = 1
+warmup = 1000
 iterations = 1000
 n_it = warmup + iterations
 
 #create data structures to store chains
 chain_mu_u_id = matrix(0,nrow= iterations, ncol = D)
+chain_lambda_u_idd = array(0,dim = c(iterations,D,D))
 chain_U_ind = array(0,dim=c(iterations,N,D))
 chain_mu_v_id = matrix(0,nrow= iterations, ncol = D)
+chain_lambda_v_idd = array(0,dim = c(iterations,D,D))
 chain_V_imd = array(0,dim=c(iterations,M,D))
 
 #################
@@ -170,102 +172,102 @@ for(it in 1:n_it){
   W_0_star = solve(W_0_star_inv)
 
   #draw Lambda_U first
-  Lambda_U = rWishart(1,v_star,W_0)
+  Lambda_U = rWishart(1,v_star,W_0_star)
   Lambda_U = Lambda_U[,,1]
   Lambda_U_inv = solve(Lambda_U)
 
-  #initiailze mu_u at random
+  #draw mu_u from its full conditional
   mu_u = chol(1/beta_star*Lambda_U_inv) %*% rnorm(D) + avg
-
-  #####
-  # U #
-  #####
-
-  for(n in 1:N){
-
-    #find sufficient stats
-    S = matrix(0,nrow = D, ncol = D)
-    for(m in 1:M){
-      if((I_nm[n,m]>0) == TRUE){
-        S = S + V[m,] %*% t(V[m,])
-      }
-    }
-
-    s = rep(0,D)
-    for(m in 1:M){
-      if((I_nm[n,m]>0) == TRUE){
-        s = s + V[m,]*R_nm[n,m]
-      }
-    }
-    #maybe I should rename them becaue S and s are similar..
-
-    #find variance and mean for normal
-    covar = Lambda_U + alpha*S
-    covar_inv = solve(covar)
-    avg = covar_inv%*%(alpha*s + Lambda_U %*% mu_u )
-
-    #draw U_n
-    U[n,] = chol(covar_inv) %*% rnorm(D) + avg
-  }
-
-  #####################
-  # mu_v and Lambda_v #
-  #####################
-
-  #find sufficient stats first
-  V_mean = apply(V,2,mean)
-  S = matrix(0,nrow = ncol(V),ncol = ncol(V))
-  for(n in 1:N){
-    S = S + V[i,] %*% t(V[i,])
-  }
-  S = S/M
-
-  #find full conditional parameters for mu_u and Lambda_u
-  beta_star = beta_0 + M
-  v_star = v_0 + M
-  avg = (beta_0*mu_u0  + M*V_mean)/(beta_star)
-  W_0_star_inv = W_0_inv + M*S + beta_0*M/beta_star * (mu_u0 - V_mean) %*% t((mu_u0 - V_mean))
-  W_0_star = solve(W_0_star_inv)
-
-  #draw Lambda_V first
-  Lambda_V = rWishart(1,v_star,W_0)
-  Lambda_V = Lambda_V[,,1]
-  Lambda_V_inv = solve(Lambda_V)
-
-  #initiailze mu_v at random
-  mu_v = chol(1/beta_star*Lambda_V_inv) %*% rnorm(D) + avg
-  
-  #####
-  # V #
-  #####
-
-  for(m in 1:M){
-
-    #find sufficient stats
-    S = matrix(0,nrow = D, ncol = D)
-    for(n in 1:N){
-      if((I_nm[n,m]>0) == TRUE){
-        S = S + U[n,] %*% t(U[n,])
-      }
-    }
-
-    s = rep(0,D)
-    for(n in 1:N){
-      if((I_nm[n,m]>0) == TRUE){
-        s = s + U[n,]*R_nm[n,m]
-      }
-    }
-    #maybe I should rename them becaue S and s are similar..
-
-    #find variance and mean for normal
-    covar = Lambda_V + alpha*S
-    covar_inv = solve(covar)
-    avg = covar_inv%*%(alpha*s + Lambda_V %*% mu_v )
-
-    #draw V_m
-    V[m,] = chol(covar_inv) %*% rnorm(D) + avg
-  }
-  
+# 
+#   #####
+#   # U #
+#   #####
+# 
+#   for(n in 1:N){
+# 
+#     #find sufficient stats
+#     S = matrix(0,nrow = D, ncol = D)
+#     for(m in 1:M){
+#       if((I_nm[n,m]>0) == TRUE){
+#         S = S + V[m,] %*% t(V[m,])
+#       }
+#     }
+# 
+#     s = rep(0,D)
+#     for(m in 1:M){
+#       if((I_nm[n,m]>0) == TRUE){
+#         s = s + V[m,]*R_nm[n,m]
+#       }
+#     }
+#     #maybe I should rename them becaue S and s are similar..
+# 
+#     #find variance and mean for normal
+#     covar = Lambda_U + alpha*S
+#     covar_inv = solve(covar)
+#     avg = covar_inv%*%(alpha*s + Lambda_U %*% mu_u )
+# 
+#     #draw U_n
+#     U[n,] = chol(covar_inv) %*% rnorm(D) + avg
+#   }
+# 
+  # #####################
+  # # mu_v and Lambda_v #
+  # #####################
+  # 
+  # #find sufficient stats first
+  # V_mean = apply(V,2,mean)
+  # S = matrix(0,nrow = ncol(V),ncol = ncol(V))
+  # for(n in 1:N){
+  #   S = S + V[i,] %*% t(V[i,])
+  # }
+  # S = S/M
+  # 
+  # #find full conditional parameters for mu_u and Lambda_u
+  # beta_star = beta_0 + M
+  # v_star = v_0 + M
+  # avg = (beta_0*mu_u0  + M*V_mean)/(beta_star)
+  # W_0_star_inv = W_0_inv + M*S + beta_0*M/beta_star * (mu_u0 - V_mean) %*% t((mu_u0 - V_mean))
+  # W_0_star = solve(W_0_star_inv)
+  # 
+  # #draw Lambda_V first
+  # Lambda_V = rWishart(1,v_star,W_0_star)
+  # Lambda_V = Lambda_V[,,1]
+  # Lambda_V_inv = solve(Lambda_V)
+  # 
+  # #initiailze mu_v at random
+  # mu_v = chol(1/beta_star*Lambda_V_inv) %*% rnorm(D) + avg
+  # 
+  # #####
+  # # V #
+  # #####
+  # 
+  # for(m in 1:M){
+  # 
+  #   #find sufficient stats
+  #   S = matrix(0,nrow = D, ncol = D)
+  #   for(n in 1:N){
+  #     if((I_nm[n,m]>0) == TRUE){
+  #       S = S + U[n,] %*% t(U[n,])
+  #     }
+  #   }
+  # 
+  #   s = rep(0,D)
+  #   for(n in 1:N){
+  #     if((I_nm[n,m]>0) == TRUE){
+  #       s = s + U[n,]*R_nm[n,m]
+  #     }
+  #   }
+  #   #maybe I should rename them becaue S and s are similar..
+  # 
+  #   #find variance and mean for normal
+  #   covar = Lambda_V + alpha*S
+  #   covar_inv = solve(covar)
+  #   avg = covar_inv%*%(alpha*s + Lambda_V %*% mu_v )
+  # 
+  #   #draw V_m
+  #   V[m,] = chol(covar_inv) %*% rnorm(D) + avg
+  # }
+  # 
   ##########
   # Record #
   ##########
@@ -275,11 +277,16 @@ for(it in 1:n_it){
     #record mu_u
     chain_mu_u_id[it - warmup,] = mu_u
 
+    chain_lambda_u_idd[it - warmup,,] = Lambda_U
+    
     #record U
     chain_U_ind[it - warmup,,] = U
 
     #record mu_v
     chain_mu_v_id[it - warmup,] = mu_v
+    
+    chain_lambda_v_idd[it - warmup,,] = Lambda_V
+    
     # record V
     chain_V_imd[it - warmup,,] = V
   }
@@ -302,46 +309,56 @@ for(d in 1:D){
 #posterior mean vs truth
 plot(true_mu_u,post_mu_u)
 
-#posterior mean for U
-post_U_nd = matrix(0,nrow = N,ncol = D)
-for(n in 1:N){
-  post_U_nd[n,] = apply(chain_U_ind[,n,],2,mean)
-}
-#chainplot
-matplot(chain_U_ind[,n,],type="l")
-#plot posterior mean vs truth
-plot(true_U[,],post_U_nd[,])
-
-#posterior mean for mu_v
-post_mu_v = apply(chain_mu_v_id,2,mean)
-#chainplot
-matplot(chain_mu_v_id,type="l")
-for(d in 1:D){
-  abline(h = true_mu_v[d])
-}
-#posterior mean vs truth
-plot(true_mu_v,post_mu_v)
-
-#posterior mean for V
-post_V_md = matrix(0,nrow = M,ncol = D)
-for(m in 1:M){
-  post_V_md[m,] = apply(chain_V_imd[,m,],2,mean)
-}
-#chainplot
-matplot(chain_V_imd[,m,],type="l")
-#plot posterior mean vs truth
-num_rat = apply(I_nm,2,sum) #need to make sure we only consider movies with lots of data...
-plot(true_V[num_rat>10,],post_V_md[num_rat>10,])
-
-
-#test how posterior means do on prediction
-post_R_nm = matrix(0,nrow = N, ncol = M)
-for(i in 1:N){
-  for(j in 1:M){
-    if( (I_nm[i,j]>0) == TRUE){
-      post_R_nm[i,j] = sum(post_U_nd[i,] * post_V_md[j,])
-    }
+#posterior mean for lambda_u
+post_Lambda_U = matrix(0,nrow = D,ncol = D)
+for(i in 1:nrow(post_Lambda_U)){
+  for(j in 1:ncol(post_Lambda_U)){
+    post_Lambda_U[i,j] = mean(chain_lambda_u_idd[,i,j])
   }
 }
-
-plot(R_nm[which(I_nm>0)], post_R_nm[which(I_nm>0)])
+plot(true_Lambda_U,post_Lambda_U)
+# 
+# #posterior mean for U
+# post_U_nd = matrix(0,nrow = N,ncol = D)
+# for(n in 1:N){
+#   post_U_nd[n,] = apply(chain_U_ind[,n,],2,mean)
+# }
+# #chainplot
+# matplot(chain_U_ind[,n,],type="l")
+# #plot posterior mean vs truth
+# plot(true_U[,],post_U_nd[,])
+# 
+# #posterior mean for mu_v
+# post_mu_v = apply(chain_mu_v_id,2,mean)
+# #chainplot
+# matplot(chain_mu_v_id,type="l")
+# for(d in 1:D){
+#   abline(h = true_mu_v[d])
+# }
+# #posterior mean vs truth
+# plot(true_mu_v,post_mu_v)
+# 
+# #posterior mean for V
+# post_V_md = matrix(0,nrow = M,ncol = D)
+# for(m in 1:M){
+#   post_V_md[m,] = apply(chain_V_imd[,m,],2,mean)
+# }
+# #chainplot
+# matplot(chain_V_imd[,m,],type="l")
+# #plot posterior mean vs truth
+# num_rat = apply(I_nm,2,sum) #need to make sure we only consider movies with lots of data...
+# plot(true_V[num_rat>10,],post_V_md[num_rat>10,])
+# 
+# 
+# #test how posterior means do on prediction
+# post_R_nm = matrix(0,nrow = N, ncol = M)
+# for(i in 1:N){
+#   for(j in 1:M){
+#     if( (I_nm[i,j]>0) == TRUE){
+#       post_R_nm[i,j] = sum(post_U_nd[i,] * post_V_md[j,])
+#       # post_R_nm[i,j] = sum(post_mu_u*post_mu_v)
+#     }
+#   }
+# }
+# 
+# plot(R_nm[which(I_nm>0)], post_R_nm[which(I_nm>0)])
