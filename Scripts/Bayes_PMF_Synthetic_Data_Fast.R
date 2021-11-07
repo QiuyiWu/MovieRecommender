7d156f1333f071e941b8820bdab76947403b604e
Patrick LeBlanc <patrick.m.leblanc@gmail.com>
2021-11-07 10:53:18 -0500

    Confirmed this works on datasets of similar size and sparsity as MovieLens subset

--- Scripts/Bayes_PMF_Synthetic_Data_Fast.R
+++ Scripts/Bayes_PMF_Synthetic_Data_Fast.R
@@ -8,9 +8,9 @@ set.seed(1)
 load("../Data Structures/cluster_mat.rda")
 
 #rename to indicator functions
-I_nm = cluster_mat[1:100,1:1000] #subset for time...
+I_nm = cluster_mat[,] #subset for time...
 #make new indicator matrix that is full, to eliminate effects of empty data
-I_nm = matrix(1,nrow = 200, ncol = 200)
+# I_nm = matrix(1,nrow = 200, ncol = 200)
 
 # recover number of users N and number of items M
 N= nrow(I_nm)
@@ -32,7 +32,7 @@ for(m in 1:M){
 rm(cluster_mat)
 
 #set D
-D = 4
+D = 2
 
 #set hyperparameters
 mu_u0 = rep(0,D)
@@ -141,7 +141,7 @@ mu_v = true_mu_v
 V = true_V
 
 #set number of iterations
-warmup = 1
+warmup = 1000
 iterations = 1000
 n_it = warmup + iterations
 
@@ -161,60 +161,60 @@ chain_V_imd = array(0,dim=c(iterations,M,D))
 
 for(it in 1:n_it){
   
-  # #####################
-  # # mu_u and Lambda_u #
-  # #####################
-  # 
-  # #find sufficient stats first
-  # U_mean = apply(U,2,mean)
-  # S = matrix(0,nrow = ncol(U),ncol = ncol(U))
-  # for(n in 1:N){
-  #   S = S + U[n,] %*% t(U[n,])
-  # }
-  # S = S/N
-  # 
-  # #find full conditional parameters for mu_u and Lambda_u
-  # beta_star = beta_0 + N
-  # v_star = v_0 + N
-  # avg = (beta_0*mu_u0  + N*U_mean)/(beta_star)
-  # W_0_star_inv = W_0_inv + N*S + beta_0*N/beta_star * (U_mean - mu_u0) %*% t((U_mean - mu_u0))
-  # W_0_star = solve(W_0_star_inv)
-  # 
-  # #draw Lambda_U first
-  # Lambda_U = rWishart(1,v_star,W_0_star)
-  # Lambda_U = Lambda_U[,,1]
-  # Lambda_U_inv = solve(Lambda_U)
-  # 
-  # #draw mu_u from its full conditional
-  # mu_u = chol(1/beta_star*Lambda_U_inv) %*% rnorm(D) + avg
-  # 
-  # #####
-  # # U #
-  # #####
-  # 
-  # for(n in 1:N){
-  #   
-  #   #find sufficient stats
-  #   S = matrix(0,nrow = D, ncol = D)
-  #   
-  #   for(m in user_rated_n[[n]]){
-  #     S = S + V[m,] %*% t(V[m,])
-  #   }
-  # 
-  #   s = rep(0,D)
-  #   for(m in user_rated_n[[n]]){
-  #     s = s + V[m,]*R_nm[n,m]
-  #   }
-  #   #maybe I should rename them becaue S and s are similar..
-  #   
-  #   #find variance and mean for normal
-  #   covar = Lambda_U + alpha*S
-  #   covar_inv = solve(covar)
-  #   avg = covar_inv%*%(alpha*s + Lambda_U %*% mu_u )
-  #   
-  #   #draw U_n
-  #   U[n,] = chol(covar_inv) %*% rnorm(D) + avg
-  # }
+  #####################
+  # mu_u and Lambda_u #
+  #####################
+
+  #find sufficient stats first
+  U_mean = apply(U,2,mean)
+  S = matrix(0,nrow = ncol(U),ncol = ncol(U))
+  for(n in 1:N){
+    S = S + U[n,] %*% t(U[n,])
+  }
+  S = S/N
+
+  #find full conditional parameters for mu_u and Lambda_u
+  beta_star = beta_0 + N
+  v_star = v_0 + N
+  avg = (beta_0*mu_u0  + N*U_mean)/(beta_star)
+  W_0_star_inv = W_0_inv + N*S + beta_0*N/beta_star * (U_mean - mu_u0) %*% t((U_mean - mu_u0))
+  W_0_star = solve(W_0_star_inv)
+
+  #draw Lambda_U first
+  Lambda_U = rWishart(1,v_star,W_0_star)
+  Lambda_U = Lambda_U[,,1]
+  Lambda_U_inv = solve(Lambda_U)
+
+  #draw mu_u from its full conditional
+  mu_u = chol(1/beta_star*Lambda_U_inv) %*% rnorm(D) + avg
+
+  #####
+  # U #
+  #####
+
+  for(n in 1:N){
+
+    #find sufficient stats
+    S = matrix(0,nrow = D, ncol = D)
+
+    for(m in user_rated_n[[n]]){
+      S = S + V[m,] %*% t(V[m,])
+    }
+
+    s = rep(0,D)
+    for(m in user_rated_n[[n]]){
+      s = s + V[m,]*R_nm[n,m]
+    }
+    #maybe I should rename them becaue S and s are similar..
+
+    #find variance and mean for normal
+    covar = Lambda_U + alpha*S
+    covar_inv = solve(covar)
+    avg = covar_inv%*%(alpha*s + Lambda_U %*% mu_u )
+
+    #draw U_n
+    U[n,] = chol(covar_inv) %*% rnorm(D) + avg
+  }
   
   #####################
   # mu_v and Lambda_v #
@@ -366,8 +366,10 @@ post_R_nm = matrix(0,nrow = N, ncol = M)
 for(n in 1:N){
   for(m in user_rated_n[[n]]){
     post_R_nm[n,m] = sum(post_U_nd[n,] * post_V_md[m,])
-    post_R_nm[n,m] = sum(true_U[n,] * true_V[m,])
+    # post_R_nm[n,m] = sum(true_U[n,] * true_V[m,])
   }
 }
+R_nm = R_nm[which(I_nm>0)]
+post_R_nm = post_R_nm[which(I_nm>0)]
 plot(R_nm[], post_R_nm[])
-# plot(R_nm[which(I_nm>0)], post_R_nm[which(I_nm>0)])
+# # plot(R_nm[which(I_nm>0)], post_R_nm[which(I_nm>0)])
