e05883e9ba25bd7222b0f142a36851791581a107
Patrick LeBlanc <patrick.m.leblanc@gmail.com>
2021-12-17 19:23:58 -0500

    Added W_nm updating
    The model is a lot worse if we have to learn the true W_nm --- don't learn any latent structure correctly.  May need to go back to the drawing board -- either to make the cluster stucture more structured (that is, take the clusters as given) or to make the probabilities more principled.

--- Scripts/Occupancy_First_Try_Synthetic.R
+++ Scripts/Occupancy_First_Try_Synthetic.R
@@ -54,7 +54,7 @@ for(k in 1:K){
     true_psi_kl[k,l] = rbeta(1,a,b)
   }
 }
-
+# 
 # #maybe set these manually?
 # true_psi_kl[1,1] = 1/5
 # true_psi_kl[1,2] = 2/5
@@ -77,6 +77,12 @@ for(k in 1:K){
     true_p_kl[k,l] = rbeta(1,c,d)
   }
 }
+
+# #maybe set these manually?
+# true_p_kl[1,1] = 1/5
+# true_p_kl[1,2] = 2/5
+# true_p_kl[2,1] = 3/5
+# true_p_kl[2,2] = 4/5
 
 #generate I_nm
 I_nm = matrix(0,nrow = N,ncol = M)
@@ -124,20 +130,29 @@ theta_l = true_theta_l
 M_m = true_M_m
 
 
+# #intiailize psi_kl randomly
+# psi_kl = matrix(0,nrow = K,ncol = L)
+# for(k in 1:K){
+#   for(l in 1:L){
+#     psi_kl[k,l] = rbeta(1,1,1)
+#   }
+# }
+#initialize psi_kl at truth
+psi_kl = true_psi_kl
 
-#intiailize psi_kl randomly
-psi_kl = matrix(0,nrow = K,ncol = L)
-for(k in 1:K){
-  for(l in 1:L){
-    psi_kl[k,l] = rbeta(1,1,1)
+#initialzie W_nm at random
+W_nm = matrix(0,nrow = N,ncol = M)
+for(n in 1:N){
+  for(m in 1:M){
+    W_nm[n,m] = sample(0:1,1)
   }
 }
-# #initialize psi_kl at truth
-# psi_kl = true_psi_kl
+# #initailize W_nm at truth
+# W_nm = true_W_nm
 
-#initailize W_nm at truth
-W_nm = true_W_nm
 
+#initialize p_kl at truth
+p_kl = true_p_kl
 
 #find counts
 
@@ -178,6 +193,7 @@ chain_u_in = matrix(0,nrow = iterations, ncol = N)
 chain_theta_il = matrix(0,nrow = iterations, ncol = L)
 chain_m_im = matrix(0,nrow = iterations,ncol = M)
 chain_psi_ikl = array(0,dim=c(iterations,K,L))
+chain_W_inm = array(0,dim=c(iterations,N,M))
 
 #################
 # Gibbs Sampler #
@@ -185,102 +201,119 @@ chain_psi_ikl = array(0,dim=c(iterations,K,L))
 
 for(it in 1:n_it){
   
-  #######
-  # Phi #
-  #######
-  for(k in 1:K){
-    phi_k[k] = rgamma(1,alpha+num_u_k[k],1)
-  }
-  phi_k = phi_k/sum(phi_k)
-
-  #######
-  # U_n #
-  #######
-  for(n in 1:N){
-    #record currentassignmnet
-    old_k = U_n[n]
-
-    #de-incremnt count
-    num_u_k[old_k] = num_u_k[old_k] - 1
-
-    #make a probablitiy vector for full conditoinal
-    #on log-scale
-    probs = rep(0,K)
-    for(k in 1:K){
-      probs[k] = sum(W_nm[n,]*log(psi_kl[k,M_m]) + (1-W_nm[n,])*log(1-psi_kl[k,M_m])) + phi_k[k]
-    }
-
-    #normalize probs
-    probs = exp(probs - max(probs))/sum(exp(probs-max(probs)))
-
-    #draw from full conditional
-    new_k = sample(1:K,1,replace =TRUE,prob = probs)
-
-    #record new value
-    U_n[n] = new_k
-
-    #re-increment count
-    num_u_k[new_k] = num_u_k[new_k] + 1
-  }
+  # #######
+  # # Phi #
+  # #######
+  # for(k in 1:K){
+  #   phi_k[k] = rgamma(1,alpha+num_u_k[k],1)
+  # }
+  # phi_k = phi_k/sum(phi_k)
+  # 
+  # #######
+  # # U_n #
+  # #######
+  # for(n in 1:N){
+  #   #record currentassignmnet
+  #   old_k = U_n[n]
+  # 
+  #   #de-incremnt count
+  #   num_u_k[old_k] = num_u_k[old_k] - 1
+  # 
+  #   #make a probablitiy vector for full conditoinal
+  #   #on log-scale
+  #   probs = rep(0,K)
+  #   for(k in 1:K){
+  #     probs[k] = sum(W_nm[n,]*log(psi_kl[k,M_m]) + (1-W_nm[n,])*log(1-psi_kl[k,M_m])) + phi_k[k]
+  #   }
+  # 
+  #   #normalize probs
+  #   probs = exp(probs - max(probs))/sum(exp(probs-max(probs)))
+  # 
+  #   #draw from full conditional
+  #   new_k = sample(1:K,1,replace =TRUE,prob = probs)
+  # 
+  #   #record new value
+  #   U_n[n] = new_k
+  # 
+  #   #re-increment count
+  #   num_u_k[new_k] = num_u_k[new_k] + 1
+  # }
+  # 
+  # #########
+  # # theta #
+  # #########
+  # for(l in 1:L){
+  #   theta_l[l]= rgamma(1,beta + num_m_l[l],1)
+  # }
+  # theta_l = theta_l/sum(theta_l)
+  # 
+  # #######
+  # # M_m #
+  # #######
+  # for(m in 1:M){
+  #   #record currentassignmnet
+  #   old_l = M_m[m]
+  # 
+  #   #de-incremnt count
+  #   num_m_l[old_l] = num_m_l[old_l] - 1
+  # 
+  #   #make a probablitiy vector for full conditoinal
+  #   #on log-scale
+  #   probs = rep(0,L)
+  #   for(l in 1:L){
+  #     probs[l] = sum(W_nm[,m]*log(psi_kl[U_n,l]) + (1-W_nm[,m])*log(1-psi_kl[U_n,l])) + theta_l[l]
+  #   }
+  # 
+  #   #normalize probs
+  #   probs = exp(probs - max(probs))/sum(exp(probs-max(probs)))
+  # 
+  #   #draw from full conditional
+  #   new_l = sample(1:L,1,replace =TRUE,prob = probs)
+  # 
+  #   #record new value
+  #   M_m[m] = new_l
+  # 
+  #   #re-increment count
+  #   num_m_l[new_l] = num_m_l[new_l] + 1
+  # }
+  # 
+  # ##########
+  # # psi_kl #
+  # ##########
+  # #recalculate counts (maybe this could be faster?)
+  # num_tot_kl = matrix(0,nrow = K,ncol = L)
+  # for(n in 1:N){
+  #   for(m in 1:M){
+  #     num_tot_kl[U_n[n],M_m[m]] = num_tot_kl[U_n[n],M_m[m]] + 1
+  #   }
+  # }
+  # num_W_kl = matrix(0,nrow = K,ncol = L)
+  # for(n in 1:N){
+  #   for(m in 1:M){
+  #     num_W_kl[U_n[n],M_m[m]] = num_W_kl[U_n[n],M_m[m]] + W_nm[n,m]
+  #   }
+  # }
+  # #sample from full conditional
+  # for(k in 1:K){
+  #   for(l in 1:L){
+  #     psi_kl[k,l] = rbeta(1,a + num_W_kl[k,l],b + num_tot_kl[k,l] - num_W_kl[k,l])
+  #   }
+  # }
   
-  #########
-  # theta #
-  #########
-  for(l in 1:L){
-    theta_l[l]= rgamma(1,beta + num_m_l[l],1)
-  }
-  theta_l = theta_l/sum(theta_l)
-
-  #######
-  # M_m #
-  #######
-  for(m in 1:M){
-    #record currentassignmnet
-    old_l = M_m[m]
-
-    #de-incremnt count
-    num_m_l[old_l] = num_m_l[old_l] - 1
-
-    #make a probablitiy vector for full conditoinal
-    #on log-scale
-    probs = rep(0,L)
-    for(l in 1:L){
-      probs[l] = sum(W_nm[,m]*log(psi_kl[U_n,l]) + (1-W_nm[,m])*log(1-psi_kl[U_n,l])) + theta_l[l]
-    }
-
-    #normalize probs
-    probs = exp(probs - max(probs))/sum(exp(probs-max(probs)))
-
-    #draw from full conditional
-    new_l = sample(1:L,1,replace =TRUE,prob = probs)
-
-    #record new value
-    M_m[m] = new_l
-
-    #re-increment count
-    num_m_l[new_l] = num_m_l[new_l] + 1
-  }
-  
-  ##########
-  # psi_kl #
-  ##########
-  #recalculate counts (maybe this could be faster?)
-  num_tot_kl = matrix(0,nrow = K,ncol = L)
+  ########
+  # W_nm #
+  ########
   for(n in 1:N){
     for(m in 1:M){
-      num_tot_kl[U_n[n],M_m[m]] = num_tot_kl[U_n[n],M_m[m]] + 1
-    }
-  }
-  num_W_kl = matrix(0,nrow = K,ncol = L)
-  for(n in 1:N){
-    for(m in 1:M){
-      num_W_kl[U_n[n],M_m[m]] = num_W_kl[U_n[n],M_m[m]] + W_nm[n,m]
-    }
-  }
-  #sample from full conditional
-  for(k in 1:K){
-    for(l in 1:L){
-      psi_kl[k,l] = rbeta(1,a + num_W_kl[k,l],b + num_tot_kl[k,l] - num_W_kl[k,l])
+      if(I_nm[n,m]==1){
+        W_nm[n,m]=1
+      } else{
+        k = U_n[n]
+        l = M_m[m]
+        probs = c(1-psi_kl[k,l],(1-p_kl[k,l])*psi_kl[k,l])
+        probs = probs/sum(probs)
+        W_nm[n,m] = sample(0:1,1,replace= FALSE,prob = probs)
+      }
     }
   }
   
@@ -302,6 +335,9 @@ for(it in 1:n_it){
     
     #record psi_kl
     chain_psi_ikl[it-warmup,,] = psi_kl
+    
+    #record W_nm
+    chain_W_inm[it-warmup,,] = W_nm
   }
   
   print(it)
@@ -339,3 +375,12 @@ plot(true_psi_kl[,2],post_psi_kl[,1],
      xlim= c(0,1),ylim = c(0,1))
 points(true_psi_kl[,1],post_psi_kl[,2])
 lines(0:1,0:1)
+
+#W_nm
+post_W_nm = matrix(0,nrow = N,ncol =M)
+for(n in 1:N){
+  for(m in 1:M){
+    post_W_nm[n,m] = mean(chain_W_inm[,n,m])
+  }
+}
+plot(true_W_nm,post_W_nm)
